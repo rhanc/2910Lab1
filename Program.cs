8be051598f2f9f100eb8c5440060df5bbc51fdae
rhanc <146234470+rhanc@users.noreply.github.com>
2024-01-25 16:33:22 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            List<VideoGame> games = CreateObjects();
+            SortGamesFromPublisher(games, "Ubisoft");
+            Console.WriteLine("The list of games above are games by Ubisoft.\nPress enter to continue.");
+            Console.ReadLine();
+            FindPublisherPercentage(games, "Ubisoft");
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            Console.WriteLine();
+            FindGamesByGenre(games, "Action");
+            Console.WriteLine("The list of games above are action games.\nPress enter to continue.");
+            Console.ReadLine();
+            FindGenrePercentage(games, "Action");
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            
+            Console.WriteLine("Please type the publisher of games you want to search for");
+            string publisher = Console.ReadLine();
+            PublisherData(games, publisher);
+            Console.WriteLine("Please type the genre of games you want to search for");
+            string genre = Console.ReadLine();
+            GenreData(games, genre);
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+        }
+
+        public static string[] ReadInData()//Question 1
+        { 
+            return File.ReadAllLines("videogames.csv");
+        }
+        public static List<VideoGame> CreateObjects()//Question 1
+        {
+            List<VideoGame> games = new List<VideoGame>();
+            string[] lines = ReadInData();
+            int LineNum = 1;
+            foreach (string line in lines)
+            {
+                if (LineNum != 1)
+                {
+                    //Console.WriteLine(line);
+                    string[] arr = line.Split(',');
+                    string name = arr[0];
+                    string platform = arr[1];
+                    int year = int.Parse(arr[2]);
+                    string genre = arr[3];
+                    string publisher = arr[4];
+                    double na_sales = double.Parse(arr[5]);
+                    double eu_sales = double.Parse(arr[6]);
+                    double jp_sales = double.Parse(arr[7]);
+                    double other_sales = double.Parse(arr[8]);
+                    double global_sales = double.Parse(arr[9]);
+                    VideoGame game = new VideoGame(name, platform, year, genre, publisher, na_sales, eu_sales, jp_sales, other_sales, global_sales);
+                    games.Add(game);
+                }
+                LineNum++;
+            }
+            return games;
+        }
+
+        public static void SortGamesFromPublisher(List<VideoGame> games, string publisher)//Question 3
+        {
+            List<VideoGame> SortedList = (from game in games
+                                          where game.Publisher == publisher
+                                          orderby game.Name ascending
+                                          select game).ToList();
+            //List<VideoGame> SortedList  = SortUsingCompareTo(games);
+            Console.WriteLine("Showing games by "+publisher);
+            foreach (VideoGame game in SortedList)
+            {
+                Console.WriteLine(game);
+
+                //Console.ReadLine();
+            }
+            //Console.ReadLine();
+        }
+        public static void FindPublisherPercentage(List<VideoGame> games, string publisher)//Question 4
+        {
+            int count = 0;
+            double percent=0;
+            List<VideoGame> SortedList = (from game in games
+                                          where game.Publisher.ToLower() == publisher.ToLower()
+
+                                          select game).ToList();
+             
+            count = SortedList.Count;
+            double result = (((double)count / (double)games.Count) * 100.00);
+            result = Math.Round(result,2);
+            int total = games.Count;
+            Console.WriteLine("Out of "+ (games.Count+1).ToString() + " games, " +count+" are developed by "+ publisher);
+            Console.WriteLine("Which is " + result+"%");
+        }
+        public static void FindGamesByGenre(List<VideoGame> games, string genre)//Question 5
+        {
+            int count = 0;
+            List<VideoGame> SortedList = (from game in games
+                                          where game.Genre.ToLower() == genre.ToLower() 
+                                          orderby game.Name ascending
+                                          select game).ToList();
+
+            count = SortedList.Count;
+            Console.WriteLine("Showing games by "+genre);
+            foreach (VideoGame game in SortedList)
+            {
+                Console.WriteLine(game);
+
+                //Console.ReadLine();
+            }
+        }
+
+
+
+        public static void FindGenrePercentage(List<VideoGame> games, string genre)//Question 6
+        {
+            int count = 0;
+            double percent = 0;
+            List<VideoGame> SortedList = (from game in games
+                                          where game.Genre.ToLower() == genre.ToLower()
+
+                                          select game).ToList();
+
+            count = SortedList.Count;
+            double result = (((double)count / (double)games.Count) * 100.00);
+            result = Math.Round(result, 2);
+            Console.WriteLine("Out of " +(games.Count + 1).ToString() + " games, " + count + " are "+genre+" games.");
+            Console.WriteLine("Which is " + result + "%");
+        }
+
+        public static void PublisherData(List<VideoGame> games, string publisher)
+        {
+            SortGamesFromPublisher(games, publisher); 
+            FindPublisherPercentage(games, publisher);
+        }
+
+        public static void GenreData(List<VideoGame> games, string Genre)
+        {
+            FindGamesByGenre(games, Genre);
+            FindGenrePercentage(games, Genre);
+        } 
+    }
+}
---
+++ VideoGame.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class VideoGame : IComparable<VideoGame>
+    {
+        private string name;
+        private string platform;
+        private int year;
+        private string genre;
+        private string publisher;
+        private double na_sales;
+        private double eu_sales;
+        private double jp_sales;
+        private double other_sales;
+        private double global_sales;
+
+        public VideoGame()
+        {
+            Name = "";
+            Platform = "";
+            Year = 0;
+            Genre = "";
+            Publisher = "";
+            Na_sales = 0;
+            Eu_sales = 0;
+            Jp_sales = 0;
+            Other_sales = 0;
+            Global_sales = 0;
+        } 
+
+        public VideoGame(string name, string platform, int year, string genre, string publisher, double na_sales, double eu_sales, double jp_sales, double other_sales, double global_sales)
+        {
+            this.Name = name;
+            this.Platform = platform;
+            this.Year = year;
+            this.Genre = genre;
+            this.Publisher = publisher;
+            this.Na_sales = na_sales;
+            this.Eu_sales = eu_sales;
+            this.Jp_sales = jp_sales;
+            this.Other_sales = other_sales;
+            this.Global_sales = global_sales;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Platform { get => platform; set => platform = value; }
+        public int Year { get => year; set => year = value; }
+        public string Genre { get => genre; set => genre = value; }
+        public string Publisher { get => publisher; set => publisher = value; }
+        public double Na_sales { get => na_sales; set => na_sales = value; }
+        public double Eu_sales { get => eu_sales; set => eu_sales = value; }
+        public double Jp_sales { get => jp_sales; set => jp_sales = value; }
+        public double Other_sales { get => other_sales; set => other_sales = value; }
+        public double Global_sales { get => global_sales; set => global_sales = value; }
+
+        public int CompareTo(VideoGame other)
+        {
+            return Name.CompareTo(other.Name);
+        } 
+
+        public override string ToString()
+        {
+            string str = "name: " + Name+"\n";
+            str += "platform: " + Platform+"\n";
+            str += "year: " + Year+"\n";
+            str += "genre: " + Genre + "\n";
+            str += "publisher: " + Publisher + "\n";
+            str += "NA Sales: " + Na_sales + "\n";
+            str += "EU Sales: " + Eu_sales + "\n";
+            str += "JP Sales: " + Jp_sales + "\n";
+            str += "Other Sales: " + Other_sales + "\n";
+            str += "Global Sales: " + Global_sales + "\n";
+            return str;
+        } 
+    }
+}
